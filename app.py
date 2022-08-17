a849035b32a76ecfb27a6ce1bf95a36e4c2bd634
Arjun Yuvaraj <arjunycoding@gmail.com>
2022-08-16 20:24:56 -0400

    added flash messages

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 # IMPORTS
-from flask import Flask, abort, request, url_for, render_template
+from flask import Flask, abort, flash, redirect, request, url_for, render_template
 
 # VARIABLES
 app = Flask(__name__)
+app.secret_key = "1qasdcvbhnji9jh8gf7yt24fgh0poijnwtfgnupiok3er"
 
 #  Home Page
+
+
 @app.route("/")
 def index():
     return render_template("index.html", title="Home")
 
 # Greeting Page
+
+
 @app.route("/greet/<name>")
 def hello_name(name):
     return render_template("greet.html", name=name, title="Hello")
 
 # Addition Calculator
+
+
 @app.route("/add", methods=["POST", "GET"])
 def add():
     result = None
@@ -22,13 +29,15 @@ def add():
         a = request.form.get("a", 0,  type=int)
         b = request.form.get("b", 0,  type=int)
         result = (a, b, a+b)
-    return render_template("add.html", result=result) 
+    return render_template("add.html", result=result)
+
 
 posts = {
     2: ('For Breakfast I Had...', 'Scrambled eggs and orange juice. It was delicious'),
     3: ('For Lunch I Had...', 'Pizza. It tasted great'),
     4: ('For Dinner I Had...', 'Sushi. It was soo good')
 }
+
 
 @app.route("/posts/<int:post_id>")
 def get_post(post_id):
@@ -42,20 +51,23 @@ def get_post(post_id):
 def get_all_posts():
     return render_template("posts/all.html", posts=posts.items())
 
+
 @app.route("/posts/new", methods=['GET', 'POST'])
 def new_post():
     if request.method == 'GET':
         return render_template("posts/new.html")
     title = request.form.get("title", None)
     content = request.form.get("content", None)
-    if not(title and content):
-        return render_template("posts/new.html", message="Please enter all of the fields")
-    
+    if not (title and content):
+        flash("Please enter all of the fields", "error")
+        return redirect(url_for('new_post'))
+
     post_id = max(posts.keys()) + 1
+    posts[post_id] = (title, content)
+    return redirect(url_for("get_post", post_id=post_id))
 
-    
 
-# CRUD: 
+# CRUD:
 # Create
 # Read
 # Update
