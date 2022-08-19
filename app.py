af07e98a950cb63e7a65ce156dcaf97f7bbc7a16
arjunyuvaraj <arjunycoding@gmail.com>
2022-08-19 12:53:04 -0400

    edited delete functionality and added update function

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, abort, flash, redirect, request, url_for, render_templa
 
 # APP
 app = Flask(__name__)
-app.secret_key = "1qasdcvbhnji9jh8gf7yt24fgh0poijnwtfgnupiok3er"
+app.secret_key = "ærjüń"
 
 # VARIABLES
 posts = {
@@ -25,7 +25,7 @@ def get_post(post_id):
     post = posts.get(post_id, None)
     if post is None:
         abort(404)
-    return render_template("get.html", post_title=post[0], post_content=post[1])
+    return render_template("get.html", post_title=post[0], post_content=post[1], post_id=post_id)
 
 
 @app.route("/posts/new", methods=['GET', 'POST'])
@@ -48,14 +48,42 @@ def new_post():
     flash("Your post has successfully been created", "alert-success")
     return redirect(url_for("get_all_posts", posts=posts.items()))
 
+@app.route("/posts/<int:post_id>/update", methods=['GET', 'POST'])
+# UPDATE POST
+def update_post(post_id):
+    if request.method == 'GET':
+        post = posts.get(post_id, None)
 
-@app.route("/posts/delete/<int:post_id>", methods=['GET', 'POST'])
+        if post == None:
+            return redirect(url_for("get_all_posts", posts=posts.items()))
+        
+        print(posts[post_id][1])
+        return render_template("new.html", post_title=posts[post_id][0], post_content=posts[post_id][1], value="Update Post", title="Update Post")
+
+
+    title = request.form.get("title", None)
+    content = request.form.get("content", None)
+    if not (title and content):
+        flash("Please enter all of the fields", "alert-danger")
+        return redirect(url_for('update_post', post_id=post_id, value="Update Post"))
+
+    posts[post_id] = (title, content)
+    flash("Your post has successfully been updated", "alert-success")
+    return redirect(url_for("get_post", post_id=post_id))
+    
+
+
+@app.route("/posts/<int:post_id>/delete", methods=['GET', 'POST'])
 # DELETE POST
 def delete_post(post_id):
-    post = posts.get(post_id, None)
-    if not post:
-        return redirect(url_for("get_all_posts"))
-    print(posts[post_id], post_id)
+    if request.method == 'GET':
+        post = posts.get(post_id, None)
+        if post is None:
+            return redirect(url_for("get_all_posts"))
+        
+        return render_template("delete.html", post_id=post_id, post_title=post[0], post_content=post[1])
+
+
     del posts[post_id]
     return redirect(url_for("get_all_posts"))
 
@@ -67,27 +95,3 @@ def delete_post(post_id):
 # Delete
 # HTTP RESPONSE CODES
 # See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#information_responses for all of the response codes
-# PREVIOUS CODE
-#  Home Page
-# @app.route("/")
-# def index():
-#     return render_template("index.html", title="Home")
-
-# Greeting Page
-
-
-# @app.route("/greet/<name>")
-# def hello_name(name):
-#     return render_template("greet.html", name=name, title="Hello")
-
-# Addition Calculator
-
-
-# @app.route("/add", methods=["POST", "GET"])
-# def add():
-#     result = None
-#     if request.method == "POST":
-#         a = request.form.get("a", 0,  type=int)
-#         b = request.form.get("b", 0,  type=int)
-#         result = (a, b, a+b)
-#     return render_template("add.html", result=result)
